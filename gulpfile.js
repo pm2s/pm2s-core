496267a768df1f1142e1d066bfd102ad1a1aac9a
s0ph1e <sonya1912@gmail.com>
2015-11-20 15:57:24 +0200

    Add jshint, use gulp instead of makefiles

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var argv = require('yargs').argv;
+var shell = require('gulp-shell');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+var src = [
+	'./src/*',
+	'./test/*'
+];
+
+gulp.task('migrate-up', shell.task('sequelize db:migrate'));
+gulp.task('migrate-down', shell.task('sequelize db:migrate:undo'));
+gulp.task('create-migration', shell.task('sequelize migration:create --name=' + argv.name));
+
+gulp.task('jshint', function () {
+	return gulp.src(src)
+		.pipe(jshint())
+		.pipe(jshint.reporter())
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function () {
+	return gulp.src(src)
+		.pipe(jscs())
+		.pipe(jscs.reporter())
+		.pipe(jscs.reporter('fail'));
+});
+
+gulp.task('test', ['jshint', 'jscs']);
